409762cda90edce668617d2b3c0265d6439fdc09
Dre <92935512+JPAVictoria@users.noreply.github.com>
2026-01-21 14:01:53 +0800

    feat: setup file structure

---
+++ bscs-ims/app/(main)/layout.jsx
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className='flex-1 flex flex-col bg-[#F8FAFC]'>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ bscs-ims/app/(main)/page.jsx
@@ -0,0 +1,5 @@
+// entry point of web (should be login here)
+
+export default function Login() {
+  return <></>
+}
