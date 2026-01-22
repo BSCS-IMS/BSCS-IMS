1cbf071161dc032f76bb83a53fd94e29c03504a2
Babbkin <133609831+babkkin@users.noreply.github.com>
2026-01-23 01:29:57 +0800

    feat : Created UI of Login

--- bscs-ims/app/(main)/login/page.jsx
+++ bscs-ims/app/(main)/login/page.jsx
@@ -1,7 +1,9 @@
 // you will start here (should be login page here)
 
-export default function LoginPage(){
-    return (
-        <>Login dito start</>
-    )
+import LoginPage from "@/app/components/LoginPage"
+
+export default function Login() {
+  return (
+    <LoginPage/>
+  )
 }
