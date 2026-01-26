f7262c44c97b2d0857ced2480c2cef13bbb97791
JPAVictoria <jpavictoria829@gmail.com>
2026-01-27 01:33:55 +0800

    chore: remove junks

--- bscs-ims/app/(main)/sample/page.jsx
+++ bscs-ims/app/(main)/sample/page.jsx
@@ -1,10 +1,7 @@
-import Sampeles from "@/app/modules/samplee/SampleText"
-
 export default function SamplePage(){
     return (
         <>
             <h1>fsafasfsa</h1>
-            <Sampeles/>
         </>
     )
 }
