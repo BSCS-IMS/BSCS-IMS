9acc5e681bcd906e9c16240052ce6917349284d9
Brave John Osio <c2059-23@itmlyceumalabang.onmicrosoft.com>
2026-02-26 00:12:39 +0800

    BO 003: ft: Toasts, loading page, auth page color and layout modifications (#81)
    * ft: added toasts and error handling. Minor login layout edits
    
    * ft: color modifications (lauth page)

--- bscs-ims/app/components/Toast.jsx
+++ bscs-ims/app/components/Toast.jsx
@@ -1,4 +1,24 @@
 // toast component frontend design here
 // think of other providers that should be used by everywhere in the system
 
-export default function Toast() {}
+'use client'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function Toast() {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  )
+}
