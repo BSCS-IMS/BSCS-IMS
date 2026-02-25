0daccae71c36ba8c6e556bd5dd19ffd0d4ee2242
Osio-BraveJohn-BSCS21A1 <c2059-23@itmlyceumalabang.onmicrosoft.com>
2026-02-25 23:54:55 +0800

    ft: color modifications (lauth page)

--- bscs-ims/app/modules/login-page/LoginPage.jsx
+++ bscs-ims/app/modules/login-page/LoginPage.jsx
@@ -140,19 +140,21 @@ export default function LoginPage() {
       {isLoading && <LoginLoader />}
       
       <div className='min-h-screen bg-linear-to-br from-gray-50 to-gray-100 flex flex-col'>
-        <div className="flex items-center justify-center md:justify-start gap-2 p-5 md:pl-20 mt-12">
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-3 p-5 md:pl-20 mt-12">
+  
           <Image
-            src="/LOGO_CLEAR.png"
-            alt="BSCS Logo"
-            width={100}
-            height={100}
-            className="object-contain"
+            src="/LOGO_CLEAr.png"
+            alt="Murang Bigas Logo"
+            width={55}
+            height={55}
+            className="object-contain mt-1"
             priority
           />
 
-          <h1 className="text-3xl md:text-4xl font-bold text-[#5A67BA]">
+          <h1 className="text-xl md:text-2xl font-bold text-[#1F384C] text-center md:text-left">
             {LOGIN_STATIC_DATA.APP_NAME}
           </h1>
+
         </div>
 
         <div className='flex-1 flex items-center justify-center px-4 pb-20'>
@@ -205,7 +207,7 @@ export default function LoginPage() {
 
               <Button
                 type='submit'
-                className='cursor-pointer w-full h-14 text-base bg-[#5A67BA] hover:bg-[#293582] text-white font-medium mt-8'
+                className='cursor-pointer w-full h-14 text-base bg-[#1F384C] hover:opacity-99 text-white font-medium mt-8'
                 disabled={isLoading}
               >
                 {isLoading ? 'Signing in...' : 'Sign In'}
@@ -215,7 +217,7 @@ export default function LoginPage() {
         </div>
 
         <div className='p-8 text-center md:text-right md:pr-16'>
-          <p className='text-[#5A67BA] text-sm'>{LOGIN_STATIC_DATA.LOGIN_FOOTER}</p>
+          <p className='text-[#1F384C] text-sm'>{LOGIN_STATIC_DATA.LOGIN_FOOTER}</p>
         </div>
       </div>
     </>
